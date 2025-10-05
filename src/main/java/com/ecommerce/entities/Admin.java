001b907e7af10399eb36cc18199fa211372141d7
MaximButmanov <but-maxim@yandex.ru>
2025-10-05 07:27:03 +0300

    refactor Admin

--- src/main/java/com/ecommerce/entities/Admin.java
+++ src/main/java/com/ecommerce/entities/Admin.java
@@ -4,25 +4,22 @@ public class Admin {
 	private int id;
 	private String name;
 	private String email;
-	private String phone;
 	private String password;
-	
+
 	public Admin() {
-		super();
 	}
 
-	public Admin(String name, String email, String phone, String password) {
-		super();
+	public Admin(int id, String name, String email, String password) {
+		this.id = id;
 		this.name = name;
 		this.email = email;
-		this.phone = phone;
 		this.password = password;
 	}
 
+	// getters / setters
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
@@ -30,7 +27,6 @@ public class Admin {
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -38,26 +34,14 @@ public class Admin {
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
 	}
 
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
 }
