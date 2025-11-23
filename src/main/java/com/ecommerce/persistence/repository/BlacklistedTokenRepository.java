a60bc8fad3994463e114694e4bb160adebdda339
MaximButmanov <but-maxim@yandex.ru>
2025-11-24 01:17:32 +0300

    add BlacklistedTokenRepository

---
+++ src/main/java/com/ecommerce/persistence/repository/BlacklistedTokenRepository.java
@@ -0,0 +1,11 @@
+package com.ecommerce.persistence.repository;
+
+import com.ecommerce.persistence.model.BlacklistedToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlacklistedTokenRepository extends JpaRepository<BlacklistedToken, Long> {
+
+    boolean existsByToken(String token);
+}
