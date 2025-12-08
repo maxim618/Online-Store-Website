acfbb27d7d685d1e270ce2f81bbc4d2c713c3cfe
MaximButmanov <but-maxim@yandex.ru>
2025-12-08 09:41:28 +0300

    refactor RegisterRequest

--- src/main/java/com/ecommerce/web/dto/RegisterRequest.java
+++ src/main/java/com/ecommerce/web/dto/RegisterRequest.java
@@ -1,10 +1,19 @@
 package com.ecommerce.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
+
+    @Email(message = "invalid email format")
+    @NotBlank(message = "email must not be empty ")
     private String email;
+
     private String name;
+    @NotBlank(message = "password not be empty")
+    @Size(min = 3, max = 50, message = "Password length must be between 3 and 50 characters")
     private String password;
 }
