27e316e4769b6657f396eb5c6e3721d7f78f7441
MaximButmanov <but-maxim@yandex.ru>
2026-01-14 11:38:31 +0300

    refactor OrderService

--- src/main/java/com/ecommerce/service/interfaces/OrderService.java
+++ src/main/java/com/ecommerce/service/interfaces/OrderService.java
@@ -4,9 +4,14 @@ import com.ecommerce.web.dto.OrderDto;
 import java.util.List;
 
 public interface OrderService {
+
     OrderDto placeOrder(Long userId);
+
     OrderDto getOrder(Long id);
+
     List<OrderDto> getUserOrders(Long userId);
+
     OrderDto getOrderForUser(Long orderId, Long requesterId, boolean admin);
+
     OrderDto getOneForUser(Long orderId, Long userId);
 }
