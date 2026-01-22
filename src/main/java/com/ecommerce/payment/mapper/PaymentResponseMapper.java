16202c8f4885e57d50b847c9d2ec33736aca151c
MaximButmanov <but-maxim@yandex.ru>
2026-01-23 00:34:40 +0300

    style: remove unused import

--- src/main/java/com/ecommerce/payment/mapper/PaymentResponseMapper.java
+++ src/main/java/com/ecommerce/payment/mapper/PaymentResponseMapper.java
@@ -2,7 +2,6 @@ package com.ecommerce.payment.mapper;
 
 import com.ecommerce.payment.domain.Payment;
 import com.ecommerce.payment.dto.PaymentInitResponseDto;
-import com.ecommerce.payment.provider.PaymentProviderResult;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
