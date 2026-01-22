27854945e9688dcfe9746b5be5d5ab3b26c009c5
MaximButmanov <but-maxim@yandex.ru>
2026-01-23 00:30:23 +0300

    feat: add findByIdempotencyKey to PaymentRepository

--- src/main/java/com/ecommerce/payment/repository/PaymentRepository.java
+++ src/main/java/com/ecommerce/payment/repository/PaymentRepository.java
@@ -3,7 +3,11 @@ package com.ecommerce.payment.repository;
 import com.ecommerce.payment.domain.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     boolean existsByOrderId(Long orderId);
+    Optional<Payment> findByIdempotencyKey(String idempotencyKey);
+
 }
